ff4ddf96340d5746b0e17b6ea5de7a3254c0795a
Majid Qureshi <mpq1990@gmail.com>
2019-03-22 15:55:49 +0100

    added pug for templating

--- status.js
+++ status.js
@@ -1,8 +1,16 @@
+const pug = require('pug');
+
 if (typeof localStorage === 'undefined' || localStorage === null) {
   const { LocalStorage } = require('node-localstorage');
   localStorage = new LocalStorage('./scratch');
 }
 
 module.exports = (req, res) => {
-  res.send(`the status is ${JSON.stringify(localStorage.getItem('success'))}`);
+  const compiledFunction = pug.compileFile('status.pug');
+
+  res.send(
+    compiledFunction({
+      status: localStorage.getItem('success')
+    })
+  );
 };
