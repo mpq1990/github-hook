ff85e3cddff3897714fdafeff7dbf40f11225870
Majid Qureshi <mpq1990@gmail.com>
2019-03-22 12:07:26 +0100

    parsing correctly

--- payload.js
+++ payload.js
@@ -1,8 +1,10 @@
 const crypto = require('crypto');
 
 // auth the secret shit
-function valid(req) {
+function validate(req) {
   // make sure we have the token on the env
+  console.log("hey buddy")
+  console.log(process.env.SECRET_TOKEN)
   const secretEnv = process.env.SECRET_TOKEN;
   if (!secretEnv) {
     return {
@@ -11,7 +13,7 @@ function valid(req) {
     };
   }
   // get the sig from git
-  const gitSig = req.get('HTTP_X_HUB_SIGNATURE');
+  const gitSig = req.get('X-Hub-Signature');
   if (!gitSig) {
     return {
       valid: false,
@@ -22,11 +24,11 @@ function valid(req) {
   // generate a new hash for comparison
   const hash = `sha1=${crypto
     .createHmac('sha1', secretEnv)
-    .update(req.body)
+    .update(JSON.stringify(req.body))
     .digest('hex')}`;
 
   // compare with the git one
-  const valid = crypto.timingSafeEqual(hash, gitSig);
+  const valid = crypto.timingSafeEqual(Buffer.from(hash), Buffer.from(gitSig));
   return {
     valid,
     message: 'validation result message'
@@ -34,6 +36,8 @@ function valid(req) {
 }
 
 module.exports = (req, res) => {
+  console.log(req.body)
+  console.log(req.body.raw)
   const validationResult = validate(req);
   if (!validationResult.valid) {
     res
@@ -41,6 +45,6 @@ module.exports = (req, res) => {
       .send(validationResult.message)
       .end();
   }
-  const readData = req.body.read;
+  const readData = JSON.stringify(req.body);
   res.status(200).send(readData);
 };
