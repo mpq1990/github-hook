5fa27324cf1da2892418e1a7b2e486f29585f736
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-22 14:01:24 +0100

    change to ten seconds

--- test/dummy.js
+++ test/dummy.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('Dummy test', function() {
   it('should pass after a given time 🎉', function(done) {
-    this.timeout(70000);
-    setTimeout(done, 60000);
+    this.timeout(20000);
+    setTimeout(done, 10000);
   });
 });
