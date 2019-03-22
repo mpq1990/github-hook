106ba74349b53ed9b31c3ad7292980fe36ea2c75
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-22 10:45:33 +0100

    parse my body baby

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('payload', (req, res, next) => {
-  const readData = JSON.parse(req.body.read);
-  console.log(readData);
-  next();
+app.post('/payload', (req, res) => {
+  const readData = req.body.read;
+  res.send(readData);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
