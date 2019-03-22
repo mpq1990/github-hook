2d64b7aa0c60514ada48b55a2e6991c7e02b1683
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-22 17:34:43 +0100

    tests passing

--- test/dummy.js
+++ test/dummy.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('Dummy test', function() {
   it('should pass after a given time 🎉', function(done) {
-    this.timeout(400);
+    this.timeout(500);
     setTimeout(done, 300);
   });
 });
