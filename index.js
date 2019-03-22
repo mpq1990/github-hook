a76b3ce40bbf2f581fd3fc3c55ff22381df1231e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-22 10:33:13 +0100

    added payload

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('payload', (req, res, next) => {
+  const readData = JSON.parse(req.body.read);
+  console.log(readData);
+  next();
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
